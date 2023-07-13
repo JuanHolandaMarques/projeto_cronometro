23ca8a4a89555990c4e36107d17b35e4f5b83bb2
Juan Holanda <123557653+JuanHolandaMarques@users.noreply.github.com>
2023-07-13 17:40:37 -0300

    finalizando js e organizando código

--- js.js
+++ js.js
@@ -1,19 +1,57 @@
-
 /* pegando minutagem */
   const milesimos = document.getElementById('milesimos')
   const segundos = document.getElementById ('segundos')
+  const minutos = document.getElementById('minutos')
 
+  /*valor da minutagem*/
   var segunds = 0
   var milesims = 0
+  var minuts = 0
 
-  function iniciarContagem() {
+  /* funções do cronometro */
+  var intervalo
 
-    if(milesims<=9){
-      milesimos.innerText = "0" + milesims
-      
-      milesims++
-    }
 
+  function iniciarContagem(){
+    intervalo = setInterval(Contagem, 10);
   }
+
+  /* contador */
+  function Contagem() {
+      milesims++
+      milesims <= 9 ? milesimos.innerText = "0" + milesims : milesimos.innerText = milesims;
+
+      if(milesims >= 60){
+        segunds ++
+        milesims = 0
+
+        segunds <= 9 ? segundos.innerText = "0" + segunds : segundos.innerText = segunds
+      }
+
+      if(segunds >= 60){
+        minuts ++
+        segunds = 0
+
+        minuts <= 9 ? minutos.innerText = "0" + minuts : minutos.innerText = minuts
+      }
+  }
+
+  function pararContagem(){
+    clearInterval(intervalo)
+    
+  }
+
+  function reiniciarContagem(){
+    clearInterval(intervalo)
+
+    milesims = 0
+    segunds = 0
+    minuts = 0
+
+    milesimos.innerText = "00"
+    segundos.innerText = "00"
+    minutos.innerText = "00"
+  }
+
 
 
